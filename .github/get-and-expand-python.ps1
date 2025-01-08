cd22a0453cab6d11abdcf5a96589ee5f8a93074e
Vasily Galkin <galkin.vv@yandex.kz>
2025-01-08 03:44:16 +0300

    swithing to presets

--- .github/get-and-expand-python.ps1
+++ .github/get-and-expand-python.ps1
@@ -1,10 +1,11 @@
 param (
+    [Parameter(Mandatory=$true)][string]$pythoninstcacheprefix,
     [Parameter(Mandatory=$true)][string]$pythoninstexe,
     [Parameter(Mandatory=$true)][string]$pythoninsturl
 )
-if (!(Test-Path "./downloaded/${pythoninstexe}")) {
-    New-Item -Type Directory -Force ./downloaded
-    Invoke-WebRequest -Uri "${pythoninsturl}/${pythoninstexe}" -OutFile "./downloaded/${pythoninstexe}"
+if (!(Test-Path "./downloaded/${pythoninstcacheprefix}/${pythoninstexe}")) {
+    New-Item -Type Directory -Force ./downloaded/${pythoninstcacheprefix}
+    Invoke-WebRequest -Uri "${pythoninsturl}/${pythoninstexe}" -OutFile "./downloaded/${pythoninstcacheprefix}/${pythoninstexe}"
 }
 #run exe and wauit for its completion
-& "downloaded/${pythoninstexe}" /quiet AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 "DefaultJustForMeTargetDir=${PSScriptRoot}\expanded" | Out-Default
+& "downloaded/${pythoninstcacheprefix}/${pythoninstexe}" /quiet AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 "DefaultJustForMeTargetDir=${PSScriptRoot}\expanded" | Out-Default
