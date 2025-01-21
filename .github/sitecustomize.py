f4f64dff49a116a59312bf845e1b35ad0f9a61e3
Vasily Galkin <galkin.vv@yandex.kz>
2025-01-21 11:55:36 +0300

    working pip 3.8

--- .github/sitecustomize.py
+++ .github/sitecustomize.py
@@ -18,40 +18,37 @@ def pip_in_zip_tune_extra_for_pip():
     if not importlib.util.find_spec("pip"):
         # no newer pip installed - add bundled pip to path with lowest priorityat the end of sys.path
         import importlib.resources, ensurepip, zipimport, _io, io
-        class ZipBytesImporter(zipimport.zipimporter):
-            paths_bytes_values = {}
-            def __init__(self, path=None):
-                if path is None:
-                    return
-                for virtual_path in ZipBytesImporter.paths_bytes_values:
-                    if path.startswith(virtual_path):
-                        self.prefix = path[len(virtual_path)+1:]
-                        self.archive = virtual_path
-                        self._files = zipimport._read_directory(self.archive)
-                        if self.prefix:
-                            self.prefix += os.path.sep
-                        return
-                raise ImportError("Not ZipBytesImporter prefix")
-        zip_bytes_importer_sample_instance = ZipBytesImporter()
+        paths_zipbytes_values = {}
+        def zip_bytes_importer_factory(path):
+            for virtual_path in paths_zipbytes_values:
+                if path.startswith(virtual_path):
+                    for p in sys.path:
+                        if p.endswith(".zip") and os.path.dirname(virtual_path).startswith(p):
+                            # make zipimporter for base archive and patch it
+                            result = zipimport.zipimporter(p)
+                            result.prefix = path[len(virtual_path)+1:]
+                            result.archive = virtual_path
+                            result._files = zipimport._read_directory(result.archive)
+                            if result.prefix:
+                                result.prefix += os.path.sep
+                            return result
+            raise ImportError("Not zip_bytes_importer_factory prefix")
 
         original_open_code = _io.open_code
         def patched_io_open_code(path):
-            bytes_data = ZipBytesImporter.paths_bytes_values.get(path)
+            bytes_data = paths_zipbytes_values.get(path)
             if bytes_data:
                 return io.BytesIO(bytes_data)
             return original_open_code(path)
 
-        sys.path_hooks.insert(0, ZipBytesImporter)
+        sys.path_hooks.insert(0, zip_bytes_importer_factory)
         _io.open_code = patched_io_open_code
 
         for f in importlib.resources.contents(ensurepip):
             if f.endswith(".whl"):
                 path_entry = os.path.join(os.path.dirname(ensurepip.__file__), f)
-                ZipBytesImporter.paths_bytes_values[path_entry] = importlib.resources.read_binary(ensurepip, f)
+                paths_zipbytes_values[path_entry] = importlib.resources.read_binary(ensurepip, f)
                 sys.path.append(path_entry)
-
-        import pip._vendor.distlib.resources
-        pip._vendor.distlib.resources.register_finder(zip_bytes_importer_sample_instance, None)
 
     if os.path.isabs(sys.executable):
         # make pip happy about scripts directory in path
